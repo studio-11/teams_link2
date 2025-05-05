36d8f8c57f74b030300dc7fec9af9758f5523e2b
studio-11 <131365144+studio-11@users.noreply.github.com>
2025-05-05 15:43:49 +0200

    Delete classes directory

--- classes/privacy/provider.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace tiny_teams_link\privacy;
-
-class provider implements \core_privacy\local\metadata\null_provider {
-    public static function get_reason(): string {
-        return 'privacy:metadata';
-    }
-}
